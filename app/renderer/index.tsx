5636d3da4f218fb7195bbee48188f684dcd73832
Piotr Dros <piotrdros@gmail.com>
2019-08-12 15:41:07 +0200

    save

--- app/renderer/index.tsx
+++ app/renderer/index.tsx
@@ -1,17 +1,8 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import Router from './router'
 import ListComponent from './components/ListComponent'
 
-const render = (Component) => {
-  ReactDOM.render(
-    <Component />,
-    document.getElementById('root')
-  )
-}
-
-// render(Router)
 ReactDOM.render(
     <ListComponent />,
     document.getElementById('root')
--- app/renderer/pages/start.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react'
-
-import ListComponent from '../components/ListComponent'
-
-export default () => (<ListComponent />)
